a410a53ea77e98cb56aeec07c2de0172c8dd217d
Jason Ge <gejun_1978@yahoo.com>
2018-09-28 14:20:16 +0800

    Use v-for to do implement a list
    Implement the list in the VUE component.
    
    Test: verified it works on CodeSandbox

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var card = new Vue({
   el: "#card",
   data: {
     title: "Dinosaur",
-    content: "<strong>Dinosaur</strong> was once ruling the earth."
+    content: "<strong>Dinosaur</strong> was once ruling the earth.",
+    items: [{ text: "Velocraptor" }, { text: "Figer" }, { text: "Stegosaurus" }]
   }
 });
