a0adad40bf7e9060eb1c97227e02f64f1d9e3036
Jason Ge <gejun_1978@yahoo.com>
2018-09-28 14:31:36 +0800

    Add event handler for list item
    Hook up onclick event to add/delete items in the VUE component.
    
    Test: verified it on codesandbox

--- script.js
+++ script.js
@@ -4,5 +4,19 @@ var card = new Vue({
     title: "Dinosaur",
     content: "<strong>Dinosaur</strong> was once ruling the earth.",
     items: [{ text: "Velocraptor" }, { text: "Figer" }, { text: "Stegosaurus" }]
+  },
+  methods: {
+    addItem: function() {
+      var input = document.getElementById("itemForm");
+      if (input.value !== "") {
+        this.items.push({
+          text: input.value
+        });
+        input.value = "";
+      }
+    },
+    deleteItem: function(index) {
+      this.items.splice(index, 1);
+    }
   }
 });
